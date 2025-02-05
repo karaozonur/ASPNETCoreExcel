6acc9389775fae478be77b51017406c12e66e616
karaozonur <karaoz.onr@gmail.com>
2025-02-05 10:00:23 +0300

    Add project files.

---
+++ ASPNETCoreExcel/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using ASPNETCoreExcel.Models;
+using ASPNETCoreExcel.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASPNETCoreExcel.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ExcelImportService _excelService;
+        public HomeController(ILogger<HomeController> logger, ExcelImportService excelService)
+        {
+            _logger = logger;
+            _excelService = excelService;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new List<Person>());
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadExcel(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                ViewBag.Error = "Lütfen bir Excel dosyası seçin.";
+                return View("Index", new List<Person>());
+            }
+
+            using (var stream = new MemoryStream())
+            {
+                await file.CopyToAsync(stream);
+                stream.Position = 0;
+
+                var data = _excelService.ImportFromExcel(stream);
+                return View("Index", data);
+            }
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASPNETCoreExcel/Services/ExcelImportService.cs
@@ -0,0 +1,33 @@
+﻿using ASPNETCoreExcel.Models;
+using ClosedXML.Excel;
+
+namespace ASPNETCoreExcel.Services
+{
+    public class ExcelImportService
+    {
+        public List<Person> ImportFromExcel(Stream fileStream)
+        {
+            var personList = new List<Person>();
+
+            using (var workbook = new XLWorkbook(fileStream))
+            {
+                var worksheet = workbook.Worksheets.First();
+                var rows = worksheet.RowsUsed();
+
+                foreach (var row in rows.Skip(1)) // İlk satır başlık olduğu için atlıyoruz
+                {
+                    var person = new Person
+                    {
+                        Id = int.Parse(row.Cell(1).GetString()),
+                        Name = row.Cell(2).GetString(),
+                        Email = row.Cell(3).GetString()
+                    };
+
+                    personList.Add(person);
+                }
+            }
+
+            return personList;
+        }
+    }
+}
